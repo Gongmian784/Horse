cbe06c64f76edb9bf1b83a00a77ca17aa7ed5335
Gong Mian <35054112+Gongmian784@users.noreply.github.com>
2023-12-03 20:01:25 +0800

    Update consensus.sh
    setMinDepth1

--- 05.maternal/cmd/consensus.sh
+++ 05.maternal/cmd/consensus.sh
@@ -3,7 +3,7 @@ export bam=$1
 export name=`basename $1 .mitochondria.*bam`
 angsd -doFasta 2 \
     -i $bam \
-    -setMinDepth 3 -minQ 25 -minMapQ 25 \
+    -minQ 25 -minMapQ 25 \
     -only_proper_pairs 0 \
     -explode 1 \
     -doCounts 1 \
